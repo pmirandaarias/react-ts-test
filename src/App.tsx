21985840e2c730686d3ce731ea8345d757d2b359
Paulo Miranda Arias <p0m067j@M-C02D5K4TMD6M.local>
2022-05-01 23:07:18 -0400

    applied eslint fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import React from 'react';
+
+import logo from './logo.svg';
+
+function App () {
   return (
     <div className="App">
       <header className="App-header">
--- src/__tests__/utilExample.test.ts
+++ src/__tests__/utilExample.test.ts
@@ -1,10 +1,10 @@
 import utilExample from '../utilExample';
 
-describe('asd', () => {
-  it('asdas', () => {
+describe('utilExample', () => {
+  it('should be false on number out of array', () => {
     expect(utilExample(5)).toBeFalsy();
   });
-  it('asdas', () => {
+  it('should be true on number in of array', () => {
     expect(utilExample(2)).toBeTruthy();
   });
 });
